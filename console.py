7e5f9193ef11be7ed178f4580d6758a121afdcce
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-10-15 18:44:41 +0300

    console

--- console.py
+++ console.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
-:wq
 
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    '''Entry point'''
+
+    prompt = "(hbnb) "
+
+
+    def do_quit(self, arg):
+        '''Quit command to exit the program'''
+        return True
+
+    def do_EOF(self, arg):
+        """Quit when Ctr+z is hit"""
+        return True
+
+    def emptyline(self):
+        """Overrides the default empty line. Empty line executes nothing"""
+    
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
