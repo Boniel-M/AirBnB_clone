432b8646f9726d59ac87ceaf770c2a69ffee609d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-10-15 22:03:41 +0300

    A

--- console.py
+++ console.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+import json
 import cmd
+from models.base_model import BaseModel
+import models.__init__
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     '''Entry point'''
 
     prompt = "(hbnb) "
-
 
     def do_quit(self, arg):
         '''Quit command to exit the program'''
@@ -18,6 +21,14 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Overrides the default empty line. Empty line executes nothing"""
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it(to the JSON file) and prints it"""
+        base1 = BaseModel()
+        FileStorage.new()
+        with open(FileStorage._file, "w") as file:
+            FileStorage.save()
+            print(base.id)
     
 
 if __name__ == '__main__':
